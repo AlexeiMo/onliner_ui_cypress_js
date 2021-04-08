290d8d3cade3c73850ec2a45d8ba25f616b1e9cb
RuslanGitelman <47743960+RuslanGitelman@users.noreply.github.com>
2021-04-08 17:43:14 +0300

    - add test data

---
+++ cypress/support/test_data/test.data.js
@@ -0,0 +1,45 @@
+module.exports = {
+    login: {
+        login: "",
+        password: ""
+    },
+    navigation: {
+        catalog: {
+            title: "Каталог",
+            url: "https://catalog.onliner.by/"
+        },
+        news: {
+            title: "Новости",
+            url: "https://www.onliner.by/"
+        },
+        ab: {
+            title: "Автобарахолка",
+            url: "https://ab.onliner.by/"
+        },
+        rent: {
+            title: "Дома и квартиры",
+            url: "https://r.onliner.by/pk/"
+        },
+        services: {
+            title: "Услуги",
+            url: "https://s.onliner.by/tasks"
+        },
+        fleaMarket: {
+            title: "Барахолка",
+            url: "https://baraholka.onliner.by/"
+        },
+        forum: {
+            title: "Форум",
+            url: "https://forum.onliner.by/"
+        }
+
+    },
+    catalog: {
+        categoryName: "Электроника",
+        subCategoryName: "Телевидение и видео",
+        productCategoryName: "Телевизоры"
+    },
+    compare: {
+        url: "https://catalog.onliner.by/compare/"
+    }
+}
